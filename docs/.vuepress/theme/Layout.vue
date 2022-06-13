98c04a1cb637621d1fcd58c8a2d7233deebbabe4
Geonhee Lee <beta1360@naver.com>
2022-06-14 07:45:29 +0900

    feat: Modify Layout component

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="theme-container">
-    <Content />
+    <Content class="theme-default-content" />
   </div>
 </template>
+
+<style lang="stylus" src="@vuepress/theme-default/styles/index.styl"></style>
