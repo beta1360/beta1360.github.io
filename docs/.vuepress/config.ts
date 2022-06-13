0cb9e9fdc795533be38638444fe45d75f01a5ecb
Geonhee Lee <beta1360@naver.com>
2022-06-14 07:33:24 +0900

    feat: Add init-setting and mock-data

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vuepress/config'
+
+export default defineConfig({
+  title: "Geonhee's Blog.",
+  description: "Geonhee's development blog!",
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }],
+    sidebar: 'auto'
+  }
+})
