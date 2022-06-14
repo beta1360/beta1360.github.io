b2fca3db56b47150ae5364d352e4c7b9a4e73194
Geonhee Lee <beta1360@naver.com>
2022-06-15 04:41:49 +0900

    feat: Renew init settings

--- docs/.vuepress/theme/Layout.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="theme-container">
-    <Content class="theme-default-content" />
-  </div>
-</template>
-
-<style lang="stylus" src="@vuepress/theme-default/styles/index.styl"></style>
--- docs/.vuepress/theme/layouts/GlobalLayout.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div id="global-layout">
-    <component :is="$page.path ? 'Layout' : 'NotFound'" />
-    <footer class="siteFooter">&copy; 개발자 이건희</footer>
-  </div>
-</template>
